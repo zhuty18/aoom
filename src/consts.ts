dc312181b0c3551aa5a78910e5dd57218c474d8b
zhutianyao <13718054285@163.com>
2025-06-17 19:08:14 +0800

    优化主题切换

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,6 @@
 const SITE_TITLE = "狡兔百窟"
 const SITE_DESCRIPTION = "兔子草的线上存档"
+const LIGHT_THEME = "mylight"
+const DARK_THEME = "mydark"
 
-export { SITE_TITLE, SITE_DESCRIPTION }
+export { SITE_TITLE, SITE_DESCRIPTION, LIGHT_THEME, DARK_THEME }
